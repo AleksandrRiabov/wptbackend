e8e3b59a981852abfb6fb39d8f118a86ff14cf6b
Aleksandr Riabov <sania.riabov@gmail.com>
2023-05-19 03:19:54 +0100

    Added more field to Options model

--- models/Options.js
+++ models/Options.js
@@ -5,6 +5,7 @@ export const optionsSchema = new mongoose.Schema(
     loadType: { type: [String], required: true },
     freightType: { type: [String], required: true },
     contractor: { type: [String], required: true },
+    crossed: { type: [String], required: true },
     products: [
       {
         name: { type: String, required: true, unique: true },
