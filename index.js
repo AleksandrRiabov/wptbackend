1b592fb6d72ac990056737585c4f7a7de1a8b8ec
Aleksandr Riabov <sania.riabov@gmail.com>
2023-06-15 20:47:39 +0100

    serviceAcc moved to .env for deploy

--- index.js
+++ index.js
@@ -12,18 +12,27 @@ import cookieParser from "cookie-parser";
 import { initializeApp } from "firebase-admin/app";
 import admin from "firebase-admin";
 import { checkAuth } from "./middleware/checkAuth.js";
-import { readFile } from "fs/promises";
 
-const serviceKey = JSON.parse(
-  await readFile(new URL("./serviceKey.json", import.meta.url))
-);
+// CONFIGURATIONS
+dotenv.config();
+const serviceKey = {
+ type: process.env.SERVICE_TYPE,
+ project_id: process.env.SERVICE_PROJECT_ID,
+ private_key_id: process.env.SERVICE_PRIVATE_KEY_ID,
+ private_key: process.env.SERVICE_PRIVATE_KEY,
+ client_email: process.env.SERVICE_CLIENT_EMAIL,
+ client_id: process.env.SERVICE_CLIENT_ID,
+ auth_uri: process.env.SERVICE_AUTH_URI,
+ token_uri: process.env.SERVICE_TOKEN_URI,
+ auth_provider_x509_cert_url: process.env.SERVICE_AUTH_PROVIDERCERT_URL,
+ client_x509_cert_url: process.env.SERVICE_CLIENT_CERT_URL,
+ universe_domain: process.env.SERVICE_UNIVERSE_DOMAIN
+}
 
 const firebase = initializeApp({
   credential: admin.credential.cert(serviceKey),
 });
 
-// CONFIGURATIONS
-dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
