a573fea5d1f70cf02ad626e36e938b3e48c79a0d
Aleksandr Riabov <sania.riabov@gmail.com>
2023-07-14 18:04:18 +0100

    Create Day, and update day data routes merged together

--- controllers/day.js
+++ controllers/day.js
@@ -1,6 +1,6 @@
 import Day from "../models/Day.js";
 
-// GET ALL DAYS INFO IN THE SPECIFIED RANGE.
+// === GET ALL DAYS INFO WITHIN THE SPECIFIED RANGE ===
 //If only start date provided, the end date by default is Today.
 // If no date range specified, it will return info for current day (Today)
 export const getDaysDataInRange = async (req, res) => {
@@ -19,45 +19,35 @@ export const getDaysDataInRange = async (req, res) => {
   }
 };
 
-//======================================
-//Create new day
-export const createDay = async (req, res) => {
+// === CREATE NEW DAY OR UPDATE EXISTING DAY ===
+export const createOrUpdateDay = async (req, res) => {
   const { date, products } = req.body;
-  console.log(req.user);
+
   try {
     const formatedDate = date?.split("-").reverse().join("-");
 
-    await Day.create({
-      date: new Date(formatedDate),
-      day: new Date(formatedDate).getDay(),
-      products,
-      editedBy: req.user.name || req.user.email,
-    });
+    // Check if the day already exists
+    const existingDay = await Day.findOne({ date: new Date(formatedDate) });
 
-    res.status(201).json({ message: `${date} Day has been created` });
+    if (existingDay) {
+      // Update the existing day
+      existingDay.products = products;
+      existingDay.editedBy = req.user.name || req.user.email;
+      await existingDay.save();
+
+      res.status(200).json({ message: `${date} Day has been updated` });
+    } else {
+      // Create a new day
+      const newDay = await Day.create({
+        date: new Date(formatedDate),
+        day: new Date(formatedDate).getDay(),
+        products,
+        editedBy: req.user.name || req.user.email,
+      });
+
+      res.status(201).json({ message: `${date} Day has been create  d` });
+    }
   } catch (error) {
     res.status(500).json({ error: true, message: error.message });
-  }
-};
-
-//=======================================
-//Update Day figures or add the product to the list
-export const updateDayFigures = async (req, res) => {
-  const { _id } = req.body;
-
-  try {
-    const foundDay = await Day.findById(_id);
-    const editedBy = [
-      ...foundDay.editedBy,
-      { name: "Developer", date: new Date() },
-    ];
-    const updatedDay = await Day.findByIdAndUpdate(_id, {
-      products: req.body.products,
-      editedBy,
-    });
-    updatedDay.save();
-    res.status(200).json({ message: "Details has been updated" });
-  } catch (error) {
-    res.status(404).json({ error: true, message: "Day does not exist" });
   }
 };
--- routes/day.js
+++ routes/day.js
@@ -1,19 +1,12 @@
 import { Router } from "express";
-import {
-  getDaysDataInRange,
-  createDay,
-  updateDayFigures,
-} from "../controllers/day.js";
+import { getDaysDataInRange, createOrUpdateDay } from "../controllers/day.js";
 import { formatDates } from "../middleware/dayRoutes.js";
 const router = Router();
 
 //Get Days Data within specified date range
 router.get("/:range?", formatDates, getDaysDataInRange);
 
-//Create new Day Data
-router.post("/", createDay);
-
-//Edit Day Data
-router.put("/", updateDayFigures);
+//Create new Day Data OR update existing day
+router.post("/", createOrUpdateDay);
 
 export default router;
