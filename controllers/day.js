2a3a6e65401f8b2cea0f0b4ff6151986e98ead6d
Aleksandr Riabov <sania.riabov@gmail.com>
2023-07-23 21:23:38 +0100

    Day GET route returnes filtered by day data if DAY query

--- controllers/day.js
+++ controllers/day.js
@@ -12,31 +12,40 @@ import { isValid, format } from "date-fns";
 //and expected cases and return requested day including those values
 export const getDaysDataInRange = async (req, res) => {
   const { dateFrom, dateTo } = req;
-  const { withStats } = req.query;
-  console.log(withStats)
+  const { withStats, day } = req.query;
 
   if (!withStats) {
+    // If no withStats query, return data, else calculate and add coefficients
+    const query = {
+      date: {
+        $gte: dateFrom,
+        $lt: dateTo,
+      },
+    };
+
+    if (day !== undefined) {
+      query.day = day; // Filter for documents where day === specific day query
+    }
+
     try {
-      const days = await Day.find({
-        date: {
-          $gte: dateFrom,
-          $lt: dateTo,
-        },
-      });
+      const days = await Day.find(query);
+      console.log(query);
       res.json(days);
     } catch (error) {
       res.status(500).json({ error: true, message: error.message });
     }
   } else {
+    // return data with coeficients
     if (!isValid(new Date(dateTo))) {
       return res.status(403).json({ message: "Invalid Date format." });
     }
 
-    const dateArray = getDatesForWeeksBefore(dateTo, 10);
+    //Dates for the same day of the week for number of weeks befor
+    const datesArray = getDatesForWeeksBefore(dateTo, 10);
 
     try {
       const numberOfWeeksSameDayData = await Day.find({
-        date: { $in: dateArray },
+        date: { $in: datesArray },
       }).exec();
       // Check if retrived data contains requested date (Day data alredy exist in DB)
       // If Notification, add it to retrived data with empty products array
@@ -45,17 +54,18 @@ export const getDaysDataInRange = async (req, res) => {
           format(
             numberOfWeeksSameDayData[numberOfWeeksSameDayData.length - 1].date,
             "yyyy-MM-dd"
-          ) !== dateArray[0]
+          ) !== datesArray[0]
         ) {
-          numberOfWeeksSameDayData.push({ date: dateArray[0], products: [] });
+          numberOfWeeksSameDayData.push({ date: datesArray[0], products: [] });
         }
 
-        const withCoefficients =
-          calculateMedianCoefficients(numberOfWeeksSameDayData);
-        const finalData = {...withCoefficients, pastData: numberOfWeeksSameDayData}
-        return res.status(200).json([finalData]);
-      }
+        const withCoefficients = calculateMedianCoefficients(
+          numberOfWeeksSameDayData
+        );
 
+        return res.status(200).json([{ ...withCoefficients }]);
+      }
+      //if nothing found return empty array
       return res.status(200).json([]);
     } catch (error) {
       return res
